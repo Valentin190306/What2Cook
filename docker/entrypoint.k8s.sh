4c400db9316f17a9d3f2873b55f05f75f256f177
MattZander24 <mattzanderm24mail.com>
2026-06-24 23:22:34 -0300

    permisos para upload img en el deployment gcp y arreglitos estéticos como siempre regalaré a este mundo

--- docker/entrypoint.k8s.sh
+++ docker/entrypoint.k8s.sh
@@ -8,4 +8,8 @@ mkdir -p /var/www/app/log/cache/translations
 chown -R www-data:www-data /var/www/app/log
 chmod -R 755 /var/www/app/log
 
+mkdir -p /var/www/app/public/uploads/avatars
+chown -R www-data:www-data /var/www/app/public/uploads
+chmod -R 775 /var/www/app/public/uploads
+
 exec /usr/bin/supervisord -c /etc/supervisor.conf
