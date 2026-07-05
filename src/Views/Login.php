e353c6885bb7e561d69f99909a093ab9bb542d23
MattZander24 <mattzanderm24mail.com>
2026-07-05 16:42:58 -0300

    Estética a Google Button

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -39,12 +39,7 @@ $noindex = true;
         <div class="form-actions">
             <button type="submit" class="btn-primary">Entrar</button>
         </div>
-    </form>
-    
-    <div class="form-divider">
-        <span>o</span>
-    </div>
-    
+    </form>    
     <a href="/auth/google" class="btn-google">
         <svg viewBox="0 0 24 24" width="20" height="20">
             <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
